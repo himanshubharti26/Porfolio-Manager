fbe8e16820665074f4b420e1df11c6e3c9f69353
Himanshu Bharti <himanshu@trusity.com>
2025-06-28 14:04:27 +0400

    resolved the bugs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser"
+
 const app = express();
 
 app.use(cors({
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 //routes import;
 import userRouter from "./routes/user.routes.js";
+import securityRouter from "./routes/security.routes.js";
 
 
 app.use("/api/v1/users", userRouter);
