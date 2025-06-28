6a4e8c06b10a9ae6278b0b64f66824e4667161f1
Himanshu Bharti <himanshu@trusity.com>
2025-06-28 14:05:41 +0400

    resolved running errors

--- src/routes/security.routes.js
+++ src/routes/security.routes.js
@@ -4,9 +4,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-// Public route to get securities
 router.route("/").get(verifyJWT, getSecurities);
-// Secured route to get securities (if needed in future)
 router.route("/:securityId").get(verifyJWT, getSecurityById);
 
 router.route("/create").post(verifyJWT,  createSecurity);
